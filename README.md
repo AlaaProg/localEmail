adccd18b074778f462b9bb2713e774145b2695e3
alaaProg <alaa.21.iraq@gmail.com>
2020-02-28 09:37:23 +0300

    update

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # localEmail
 Local Server Email 
+
+
+# How to use 
+### Install 
+	$ python -m pip install -r requirements.txt
+
+### Run 
+	$ python serve.py 
--- server.py
+++ server.py
@@ -13,6 +13,5 @@ if __name__ == '__main__':
 
 	db.connect()
 	models.create_table(db)
-	# models.Email.delete().execute()
 	executor.submit(server.run)
 	app.run(port=config.site.port, debug=config.site.debug, use_reloader=config.site.debug)
